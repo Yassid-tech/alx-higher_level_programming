a996839bd157fd3976fc4ff33dd6fed9424e8ccd
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2024-01-23 17:27:50 +0100

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,7 +7,7 @@ class Square:
     Class that defines properties of square by: (based on 5-square.py).
 
     Attributes:
-        size: size of a square (1 side).
+        size: size of a square.
     """
     def __init__(self, size=0, position=(0, 0)):
         """Creates new instances of square.
